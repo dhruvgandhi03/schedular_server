de7883fe4620e1adc33c48d33853c27e07d00f8c
Dhruv <gandhidhruv388@gmail.com>
2024-01-07 12:35:27 +0530

    7/01

--- model/TaskSchema.js
+++ model/TaskSchema.js
@@ -1,23 +1,28 @@
 const mongoose = require("mongoose");
 
-const taskSchema = new mongoose.Schema({
-  Taskname: {
-    type: String,
-    required: true,
+const taskSchema = new mongoose.Schema(
+  {
+    Taskname: {
+      type: String,
+      required: true,
+    },
+    Taskdes: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: String,
+      required: true,
+    },
+    time: {
+      type: String,
+      required: true,
+    },
   },
-  Taskdes: {
-    type: String,
-    required: true,
-  },
-  date: {
-    type: String,
-    required: true,
-  },
-  time: {
-    type: String,
-    required: true,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const Task = mongoose.model("Task", taskSchema);
 
